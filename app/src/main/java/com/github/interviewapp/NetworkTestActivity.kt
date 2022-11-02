05223151401ad8511ce2acd01ad8bb487205fffa
lightverse <lw2014xd@gmail.com>
2022-11-02 22:44:24 +0800

    使用默认的实现retrofit-无额外配置

--- app/src/main/java/com/github/interviewapp/NetworkTestActivity.kt
+++ app/src/main/java/com/github/interviewapp/NetworkTestActivity.kt
@@ -1,7 +1,7 @@
 package com.github.interviewapp
 
-import android.app.Activity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,13 +16,24 @@ class NetworkTestActivity : AppCompatActivity() {
         setContentView(R.layout.activity_network_test)
         supportActionBar?.title = "网络测试"
         showText = findViewById(R.id.request_result)
-        findViewById<Button>(R.id.do_request).setOnClickListener {
-            val weiboHot = SimpleRetrofit.getServices().weiboHot()
+        showText.movementMethod = ScrollingMovementMethod.getInstance()
+        findViewById<Button>(R.id.do_weibo_hot).setOnClickListener {
+            val weiboHot = SimpleRetrofit.apiService.weiboHot()
             Thread {
                 val get = weiboHot.get()
                 val code = get.code()
                 runOnUiThread {
                     showText.text = get.body()?.string()
+                }
+            }.start()
+        }
+
+        findViewById<Button>(R.id.do_bli_hot).setOnClickListener {
+            val biliHot = SimpleRetrofit.apiService.bilihot()
+            Thread {
+                var res = biliHot.execute()
+                runOnUiThread {
+                    showText.text = res.body()?.string()
                 }
             }.start()
         }
--- core/src/main/java/com/github/core/network/SimpleRetrofit.kt
+++ core/src/main/java/com/github/core/network/SimpleRetrofit.kt
@@ -2,6 +2,7 @@ package com.github.core.network
 
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.GET
@@ -20,33 +21,40 @@ interface ApiService{
     @GET(value = "resou")
     fun weiboHot():CompletableFuture<Response<ResponseBody>> //如果都是默认 返回值可以是这个
 
+    @GET(value = "bilihot")
+    fun bilihot():Call<ResponseBody>
+}
 
+interface JsonResultService{
+
+    @GET(value = "resou")
+    fun weiboHot():CompletableFuture<Response<ResponseBody>> //如果都是默认 返回值可以是这个
+
+}
+
+
+fun createRetrofitBuilder():Retrofit.Builder{
+    val DEFAULT_TIMEOUT:Long = 10
+    val BASE_URL:String = "https://tenapi.cn"
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+        .retryOnConnectionFailure(true)
+        .build()
+    //如果初始化retrofit没配置 返回值如何解析 默认只支持两种返回值 CompleteFuture和Call,如果写其他会报错
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+    return retrofit
 }
 
 
 object SimpleRetrofit{
 
-    private const val DEFAULT_TIMEOUT:Long = 10
-    private const val BASE_URL:String = "https://tenapi.cn"
-
-    private val apiService:ApiService
-
-    init {
-        val okHttpClient = OkHttpClient.Builder()
-            .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-            .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-            .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
-            .retryOnConnectionFailure(true)
-            .build()
-        //如果初始化retrofit没配置 返回值如何解析 默认只支持两种返回值 CompleteFuture和Call,如果写其他会报错
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .build()
-        apiService = retrofit.create(ApiService::class.java)
+    val apiService by lazy {
+        createRetrofitBuilder().build().create(ApiService::class.java)
     }
-
-    fun getServices() = apiService
 
 }
 
