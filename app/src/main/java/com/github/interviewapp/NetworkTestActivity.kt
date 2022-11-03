3e3f2433dc80f04535f17c1da5b944cfd665850c
lightverse <lw2014xd@gmail.com>
2022-11-03 23:21:13 +0800

    完成retrofit calladapter添加

--- app/src/main/java/com/github/interviewapp/NetworkTestActivity.kt
+++ app/src/main/java/com/github/interviewapp/NetworkTestActivity.kt
@@ -3,39 +3,88 @@ package com.github.interviewapp
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.github.core.network.ResultBack
 import com.github.core.network.SimpleRetrofit
+import com.github.core.network.StringResultRetrofit
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.concurrent.CompletableFuture
 
 class NetworkTestActivity : AppCompatActivity() {
 
     lateinit var showText:TextView
+
+    lateinit var stringConvert:CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network_test)
         supportActionBar?.title = "网络测试"
         showText = findViewById(R.id.request_result)
+        stringConvert = findViewById(R.id.use_string_convert)
         showText.movementMethod = ScrollingMovementMethod.getInstance()
         findViewById<Button>(R.id.do_weibo_hot).setOnClickListener {
-            val weiboHot = SimpleRetrofit.apiService.weiboHot()
-            Thread {
-                val get = weiboHot.get()
-                val code = get.code()
-                runOnUiThread {
-                    showText.text = get.body()?.string()
-                }
-            }.start()
+
+            if(stringConvert.isChecked){
+                Thread {
+                    try {
+                        val weiboHot = StringResultRetrofit.apiService.weiboHot()
+                        runOnUiThread {
+                            showText.text = weiboHot.toString()
+                        }
+                    }catch (e:Exception){
+                        println(e.stackTrace)
+                    }
+
+                }.start()
+            }else{
+                Thread {
+                    try {
+                        val weiboHot = SimpleRetrofit.apiService.weiboHot()
+                        val res = weiboHot.get()
+                        runOnUiThread {
+                            showText.text = res.body()?.string()
+                        }
+                    }catch (e:Exception){
+                        println(e.stackTrace)
+                    }
+
+                }.start()
+            }
+
         }
 
         findViewById<Button>(R.id.do_bli_hot).setOnClickListener {
-            val biliHot = SimpleRetrofit.apiService.bilihot()
-            Thread {
-                var res = biliHot.execute()
-                runOnUiThread {
-                    showText.text = res.body()?.string()
-                }
-            }.start()
+            if(stringConvert.isChecked){
+                val biliHot = StringResultRetrofit.apiService.bilihot()
+                biliHot.run(object :ResultBack<String>{
+                    override fun onResult(t: String) {
+                        showText.text = t
+                    }
+
+                })
+            }else{
+                val biliHot = SimpleRetrofit.apiService.bilihot()
+                biliHot.enqueue(object :Callback<ResponseBody>{
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        showText.text = response.body().toString()
+                    }
+
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+
+                    }
+
+                })
+            }
+
         }
     }
 }
--- core/src/main/java/com/github/core/network/SimpleRetrofit.kt
+++ core/src/main/java/com/github/core/network/SimpleRetrofit.kt
@@ -2,10 +2,10 @@ package com.github.core.network
 
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.http.GET
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 
@@ -22,13 +22,16 @@ interface ApiService{
     fun weiboHot():CompletableFuture<Response<ResponseBody>> //如果都是默认 返回值可以是这个
 
     @GET(value = "bilihot")
-    fun bilihot():Call<ResponseBody>
+    fun bilihot():Call<ResponseBody> //使用默认BuiltInConverters
 }
 
 interface JsonResultService{
 
     @GET(value = "resou")
-    fun weiboHot():CompletableFuture<Response<ResponseBody>> //如果都是默认 返回值可以是这个
+    fun weiboHot():MyJob<String> //返回不是ResponseBody了但是Response还在
+
+    @GET(value = "bilihot")
+    fun bilihot():MyJob<String>
 
 }
 
@@ -56,5 +59,74 @@ object SimpleRetrofit{
         createRetrofitBuilder().build().create(ApiService::class.java)
     }
 
+}
+
+class StringCallAdapterFactory:CallAdapter.Factory(){
+    override fun get(
+        returnType: Type,
+        annotations: Array<out Annotation>,
+        retrofit: Retrofit
+    ): CallAdapter<*, *>? {
+        val rawType = getRawType(returnType)
+        val parameterUpperBound = getParameterUpperBound(0,returnType as ParameterizedType)
+        return StringCallAdapter<Any>(parameterUpperBound)
+    }
+
+
+}
+interface MyJob<T>{
+    fun run(res:ResultBack<T>)
+}
+
+interface ResultBack<T>{
+    fun onResult(t:T)
+}
+
+class StringCallAdapter<R>(val returnType: Type) :CallAdapter<R,MyJob<R>>{
+    override fun responseType(): Type  = returnType
+    override fun adapt(call: Call<R>): MyJob<R> {
+        return object :MyJob<R>{
+            override fun run(res: ResultBack<R>) {
+                call.enqueue(object :Callback<R>{
+                    override fun onResponse(call: Call<R>, response: Response<R>) {
+                        //这里实现了将响应的泛型 和 返回值泛型结合 注意此处泛型设计
+                        res.onResult(response.body()!!)
+                    }
+
+                    override fun onFailure(call: Call<R>, t: Throwable) {
+                    }
+                })
+            }
+
+        }
+    }
+}
+class StringConvertFactory : Converter.Factory() {
+    override fun responseBodyConverter(
+        type: Type,
+        annotations: Array<out Annotation>,
+        retrofit: Retrofit
+    ): Converter<ResponseBody, *>? {
+        if(getRawType(type) != String::class.java){
+            return null
+        }
+        return StringConvert()
+    }
+
+    class StringConvert:Converter<ResponseBody,String>{
+        override fun convert(value: ResponseBody): String? {
+            return value.string()
+        }
+    }
+}
+
+
+object StringResultRetrofit{
+    val apiService by lazy {
+        createRetrofitBuilder()
+            .addConverterFactory(StringConvertFactory())
+            .addCallAdapterFactory(StringCallAdapterFactory())
+            .build().create(JsonResultService::class.java)
+    }
 }
 
